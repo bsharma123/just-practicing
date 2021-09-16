dd61d6bffdc34db517aef454e711963dc92db947
Bipan Sharma <sharma.bipan05@gmail.com>
2021-09-16 22:37:05 +1000

    List completed

--- ex2.py
+++ ex2.py
@@ -7,9 +7,20 @@
 ##
 #print("Thank you")
 
-cube = [number ** 3 for number in range(1,11)]
-print(cube)
+colors = (1, 2, 3, 4, 5)
 
-a = list(range(1,10000))
-for number in a[0:500:5]:
-    print(number.sort())
+
+for color in colors:
+    print(f"My favorite color is: ", color)
+
+my_food = ['honey', 'chocolate', 'egg']
+friend_food = my_food[:]
+
+print("My favorite food is ")
+print(my_food)
+
+friend_food.append('caramel')
+print("My friend's favorite food is ")
+print(friend_food)
+
+
