c50d24e52f3175422d243fa9b5b87cf25e45232e
Bipan Sharma <sharma.bipan05@gmail.com>
2021-09-16 20:22:24 +1000

    up till list

--- ex1.py
+++ ex1.py
@@ -1,1 +1,19 @@
-#this is a test to add new file to a git
+places = ['london', 'iceland', 'africa', 'antartica', 'nepal']
+
+print(places)
+
+print(sorted(places))
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+places.sort(reverse = True)
+print(places)
+
+print(len(places))
