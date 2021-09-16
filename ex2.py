248eb33a372ec11e93d0c5d675263cf89ae65f7b
bsharma123 <60082269+bsharma123@users.noreply.github.com>
2021-09-16 22:39:45 +1000

    Bipan (#4)
    * addition (#1)
    
    * Bipans branch (#2)
    
    * addition
    
    * new file
    
    * ashma
    
    * up till list
    
    * List completed

--- ex2.py
+++ ex2.py
@@ -1,3 +1,4 @@
+
 #fruits=['apple', 'orange', 'tomatoes', 'bananas']
 #names = ['david', 'blake', 'mike']
 #for fruit in fruits:
@@ -7,9 +8,26 @@
 ##
 #print("Thank you")
 
+colors = (1, 2, 3, 4, 5)
+
+
+for color in colors:
+    print(f"My favorite color is: ", color)
+
+my_food = ['honey', 'chocolate', 'egg']
+friend_food = my_food[:]
+
+print("My favorite food is ")
+print(my_food)
+
+friend_food.append('caramel')
+print("My friend's favorite food is ")
+print(friend_food)
+
 cube = [number ** 3 for number in range(1,11)]
 print(cube)
 
 a = list(range(1,10000))
 for number in a[0:500:5]:
     print(number.sort())
+
